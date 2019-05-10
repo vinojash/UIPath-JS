52284c7cc455a75fcc1a344013c61d2d7486b6b8
DESKTOP-CC125MC\VinoJash <vinojash@live.com>
2019-05-10 19:03:14 +0400

    initial commit

---
+++ Helper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using EdgeJs;
+
+namespace RunJavaScript
+{
+    class Helper
+    {
+        private string data, jsFunction;
+
+        private string buildFunction()
+        {
+            return @"return function(data, callback){" + jsFunction + "callback(null, data);}";
+        }
+
+        private string execute()
+        {
+            Func<object, Task<object>> myFunc = Edge.Func(buildFunction());
+            return (string)myFunc(data).Result; ;
+        }
+
+        private void setData(string data)
+        {
+            this.data = data;
+        }
+
+        private void setJSFunction(string jsFunction)
+        {
+            this.jsFunction = jsFunction;
+        }
+
+        public string executeJSFunction(string data, string jsFunction)
+        {
+            this.setData(data);
+            this.setJSFunction(jsFunction);
+            return execute();
+        }
+    }
+}
---
+++ execuiteJavaScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Activities;
+using System.ComponentModel;
+
+namespace RunJavaScript
+{
+    public class execuiteJavaScript : CodeActivity
+    {
+        [Category("Input")]
+        [RequiredArgument]
+        public InArgument<string> Data { get; set; }
+
+        [Category("Input")]
+        public InArgument<string> JSFunction { get; set; }
+
+        [Category("Output")]
+        public OutArgument<string> Result { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            Helper runJS = new Helper();
+
+            var data = Data.Get(context);
+            var jsFunction = JSFunction.Get(context);
+
+            var result = runJS.executeJSFunction(data, jsFunction);
+            Result.Set(context, result);
+        }
+    }
+}
